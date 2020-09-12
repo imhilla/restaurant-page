301a3a55cc651cfe123c829d57620f8e4f28f559
imhilla <hillaryodhiambo282@gmail.com>
2020-09-12 10:43:10 +0300

    add semi colons to index js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Popper from 'popper.js';
+import Popper from 'popper';
 import './style.css';
-import navBarModule from './nav.js';
-import aboutModule from './about.js';
-import trendingModule from './trending.js';
-import locationModule from './location.js';
-import logo from './logo.js';
-import buttonModule from './order.js';
-import menuModule from './menu.js';
-import defaultPageModule from './default.js';
-import aboutFoodModule from './aboutourfood.js';
-import discoverCampModule from './discover.js';
+import navBarModule from './nav';
+import aboutModule from './about';
+import trendingModule from './trending';
+import locationModule from './location';
+import logo from './logo';
+import buttonModule from './order';
+import menuModule from './menu';
+import defaultPageModule from './default';
+import aboutFoodModule from './aboutourfood';
+import discoverCampModule from './discover';
 import campLocationModule from './ourLocation';
 import sander from './img/sander.jpg';
 import homepage from './img/homepage.jpg';
@@ -23,30 +23,30 @@ import golden from './img/golden.jpg';
 
 
 const clearDefaultPage = () => {
-  const clearDefault = document.getElementById('default1')
+  const clearDefault = document.getElementById('default1');
   clearDefault.style.display = 'none';
 };
 
 const clearAboutPage = () => {
-  const clearAbout = document.getElementById('aboutfood1')
+  const clearAbout = document.getElementById('aboutfood1');
   clearAbout.style.display = 'none';
 };
 clearAboutPage();
 
 const clearDiscoverPage = () => {
-  const clearDiscover = document.getElementById('campdiscover')
+  const clearDiscover = document.getElementById('campdiscover');
   clearDiscover.style.display = 'none';
 };
 clearDiscoverPage();
 
 const clearCampPage = () => {
-  const clearCamp = document.getElementById('camplocate')
+  const clearCamp = document.getElementById('camplocate');
   clearCamp.style.display = 'none';
 };
 clearCampPage();
 
 const showDefaultPage = () => {
-  const showDefault = document.getElementById('default1')
+  const showDefault = document.getElementById('default1');
   showDefault.style.display = 'block';
   clearAboutPage();
   clearDiscoverPage();
@@ -55,7 +55,7 @@ const showDefaultPage = () => {
 document.getElementById('defaultId').addEventListener('click', showDefaultPage);
 
 const showAboutPage = () => {
-  const showAbout = document.getElementById('aboutfood1')
+  const showAbout = document.getElementById('aboutfood1');
   showAbout.style.display = 'block';
   clearDiscoverPage();
   clearCampPage();
@@ -64,7 +64,7 @@ const showAboutPage = () => {
 document.getElementById('aboutEvent').addEventListener('click', showAboutPage);
 
 const showDiscoverPage = () => {
-  const showDiscover = document.getElementById('campdiscover')
+  const showDiscover = document.getElementById('campdiscover');
   showDiscover.style.display = 'block';
   clearCampPage();
   clearDefaultPage();
@@ -73,7 +73,7 @@ const showDiscoverPage = () => {
 document.getElementById('discoverr').addEventListener('click', showDiscoverPage);
 
 const showCampPage = () => {
-  const showCampPage = document.getElementById('camplocate')
+  const showCampPage = document.getElementById('camplocate');
   showCampPage.style.display = 'block';
   clearDefaultPage();
   clearAboutPage();
